51936ea05c7e763e6b51c289f44961250c098ef9
TreeB7337 <118823222+TreeB7337@users.noreply.github.com>
2024-02-05 20:52:25 +0530

    first commit

---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/sarve/Desktop/machine learning/heart-disease-project/env/lib"
+XSLT_LIBS="-lxslt -L/Users/sarve/Desktop/machine learning/heart-disease-project/env/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/sarve/Desktop/machine learning/heart-disease-project/env/include"
+MODULE_VERSION="xslt-1.1.37"
